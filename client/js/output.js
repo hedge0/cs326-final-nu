6a94576e064a4ec63abd2a630b307f1229f78af7
Elias Martin <elias@sportsvisio.com>
2021-11-02 21:23:48 -0400

    update

--- client/js/output.js
+++ client/js/output.js
@@ -1,14 +1,6 @@
-let myArray = [
-    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
-    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
-    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
-    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
-    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
-    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
-    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'}
-]
-
-createTable(myArray)
+$(document).ready(function() {
+    $('#example').DataTable();
+} );
 
 document.getElementById('analyze').addEventListener('click', () => {
     location.href = "input.html";
@@ -17,16 +9,3 @@ document.getElementById('analyze').addEventListener('click', () => {
 document.getElementById('signout').addEventListener('click', () => {
     location.href = "login.html";
 });
-
-function createTable(data){
-    let table = document.getElementById('table')
-    for (let i = 0; i < data.length; i++){
-        let row = `<tr>
-                        <td>${data[i].Text}</td>
-                        <td>${data[i].Sentiment}</td>
-                        <td>${data[i].Languages}</td>
-                        <td>${data[i].Date}</td>
-                  </tr>`;
-        table.innerHTML += row;
-    }
-}
