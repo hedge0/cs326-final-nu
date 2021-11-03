595e438b9a9d7442ae8fc414f9757b0d764dec26
Elias Martin <elias@sportsvisio.com>
2021-11-02 21:45:43 -0400

    table somewhat finished, still more to do

--- client/js/output.js
+++ client/js/output.js
@@ -1,5 +1,41 @@
+let myArray = [
+    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'90', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'50', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'70', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'90', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'50', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'70', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'90', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'50', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'70', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'90', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'50', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'70', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'90', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'50', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'70', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'90', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'50', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'70', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'80', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'90', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'50', 'Languages':'English', 'Date':'10/14/1990'},
+    {'Text':'RECORD HIGH', 'Sentiment':'70', 'Languages':'English', 'Date':'10/14/1990'}
+]
+
+createTable(myArray)
+
 $(document).ready(function() {
-    $('#example').DataTable();
+    $('#data').DataTable();
 } );
 
 document.getElementById('analyze').addEventListener('click', () => {
@@ -9,3 +45,17 @@ document.getElementById('analyze').addEventListener('click', () => {
 document.getElementById('signout').addEventListener('click', () => {
     location.href = "login.html";
 });
+
+
+function createTable(data){
+    let table = document.getElementById('table')
+    for (let i = 0; i < data.length; i++){
+        let row = `<tr>
+                        <td>${data[i].Text}</td>
+                        <td>${data[i].Sentiment}</td>
+                        <td>${data[i].Languages}</td>
+                        <td>${data[i].Date}</td>
+                  </tr>`;
+        table.innerHTML += row;
+    }
+} 
