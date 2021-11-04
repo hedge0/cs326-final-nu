b16b43066f4518743376a6b88cc41f3901fb455e
brao980 <b.rao980@gmail.com>
2021-11-04 03:13:41 -0400

    Update login.js

--- client/js/login.js
+++ client/js/login.js
@@ -1,14 +1,23 @@
 document.getElementById('login').addEventListener('click', () => {
-    if (true) {
-        location.href = "input.html";
-    }
+    location.href = "input.html";
 });
-
+async function checklogin() {
+    let response = await fetch(window.location.href);
+    if (response.ok) {
+        let result = response.json;
+        if (result.length != 2) {
+            document.getElementById('errorname').innerHTML = "enter both username and password";
+        }
+        else {
+            document.getElementById('errorname').innerHTML = "";
+        }
+    }
+}
 document.getElementById('signup').addEventListener('click', () => {
     location.href = "signup.html";
 });
 
-document.getElementById("show-password").addEventListener('change', function() {
+document.getElementById("show-password").addEventListener('change', function () {
     const password_fields = document.getElementsByName("password");
     if (this.checked) {
         for (const field of password_fields) {
@@ -20,3 +29,8 @@ document.getElementById("show-password").addEventListener('change', function() {
         }
     }
 });
+// let http = require('http');
+// http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end();
+// }).listen(8080);
