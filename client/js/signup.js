c01ae37ebcd4d1c469b6eff94c271f73cd0f853b
brao980 <b.rao980@gmail.com>
2021-11-05 00:11:50 -0400

    Update signup.js

--- client/js/signup.js
+++ client/js/signup.js
@@ -1,5 +1,28 @@
 document.getElementById('signup').addEventListener('click', () => {
-    location.href = "input.html";
+    const response = await fetch('https://.com', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify({
+            username: document.getElementById("username").value,
+            password: document.getElementById("password").value,
+            confirmpassword: document.getElementById("confirmpassword").value
+        })
+    });
+
+    if (response.ok) {
+        const responseJSON = await response.json();
+        if (responseJSON.valid) {
+            if (password === confirmpassword) {
+                window.id = responseJSON.id;
+                location.href = "login.html";
+            }
+            else {
+                alert("these passwords don't match. try again.")
+            }
+        }
+    }
 });
 
 document.getElementById("show-password").addEventListener('change', () => {
@@ -8,7 +31,7 @@ document.getElementById("show-password").addEventListener('change', () => {
         for (const field of password_fields) {
             field.type = 'username';
         }
-    } 
+    }
     else {
         for (const field of password_fields) {
             field.type = 'password';
