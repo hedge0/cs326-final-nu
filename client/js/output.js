fd9c2a41563b4fa17288f9ec32f3fab4ce775250
asun2 <andrewsun2@gmail.com>
2021-11-23 11:43:16 -0500

    download button fixes

--- client/js/output.js
+++ client/js/output.js
@@ -33,60 +33,6 @@ document.getElementById('signout').addEventListener('click', () => {
 
 
 
-function convertToCSV(objArray) {
-    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-    var str = '';
-
-    for (var i = 0; i < array.length; i++) {
-        var line = '';
-        for (var index in array[i]) {
-            if (line != '') line += ','
-
-            line += array[i][index];
-        }
-
-        str += line + '\r\n';
-    }
-
-    return str;
-}
-
-function exportCSVFile(headers, items, fileTitle) {
-    if (headers) {
-        items.unshift(headers);
-    }
-
-    // Convert Object to JSON
-    var jsonObject = JSON.stringify(items);
-
-    var csv = this.convertToCSV(jsonObject);
-
-    var exportedFilenmae = fileTitle + '.csv' || 'export.csv';
-
-    var blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
-    if (navigator.msSaveBlob) { // IE 10+
-        navigator.msSaveBlob(blob, exportedFilenmae);
-    } else {
-        var link = document.createElement("a");
-        if (link.download !== undefined) { // feature detection
-            // Browsers that support HTML5 download attribute
-            var url = URL.createObjectURL(blob);
-            link.setAttribute("href", url);
-            link.setAttribute("download", exportedFilenmae);
-            link.style.visibility = 'hidden';
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        }
-    }
-}
-
-const headers = {
-    text: 'Text',
-    sentiment: "Sentiment",
-    language: "Language",
-    date: "Date"
-}
 
 document.getElementById('download').addEventListener('click', () => {
     const response = await fetch(`https://sentiment-analyzer-team-nu.herokuapp.com/getUserLog/${storage.getItem("username")}`, {
@@ -99,11 +45,24 @@ document.getElementById('download').addEventListener('click', () => {
     if (response.ok) {
         const responseJSON = await response.json();
         if (responseJSON.valid) {
-            var csv = JSON2CSV(json);
+            const headers = {
+                text: 'Text',
+                sentiment: "Sentiment",
+                language: "Language",
+                date: "Date"
+            }
+            let json = JSON.parse(responseJSON.data);
+            json.unshift(headers);
+            let csv = JSON2CSV(json);
+            var downloadLink = document.createElement("a");
             var blob = new Blob(["\ufeff", csv]);
             var url = URL.createObjectURL(blob);
             downloadLink.href = url;
             downloadLink.download = "data.csv";
+            
+            document.body.appendChild(downloadLink);
+            downloadLink.click();
+            document.body.removeChild(downloadLink);
         }
     }
 })
